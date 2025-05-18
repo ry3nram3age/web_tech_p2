485f2155821a1640f66946e822cbea6b89e2a232
Thomas Federico <105926570@student.swin.edu.au>
2025-05-18 15:42:22 +1000

    Formatting. Added Auther Meta Tag. Changed Title. Added Formatting with Comments.

--- web_tech_prj-main/php/manage.php
+++ web_tech_prj-main/php/manage.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta auther="Thomas Federico"> <!-- if anyone makes additions to this page, please put name here -->
+        <title>HR Page</title> <!-- Temporary Title for now -->
+    </head>
+
+<!-------- HEADER --------->
+    <header>
+
+    </header>
+<!------------------------->
+
+<!---------- BODY --------->
+    <body>
+        
+    </body>
+<!------------------------->
 </html>
