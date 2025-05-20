dfa183aa37f64787ca23b3c1cd00b903d8929965
aFroginaSuit <105910146@student.swin.edu.au>
2025-05-20 11:36:33 +1000

    indented code

--- web_tech_prj-main/php/process_eoi.php
+++ web_tech_prj-main/php/process_eoi.php
@@ -3,14 +3,16 @@
 //AUTHOR - Max Dinon, 
 //Security session id stuff so users cant access this page
 
-function test_input($data) {
+function test_input($data) 
+{
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
-  }
+}
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
   $jobReferenceNumber = test_input($_POST['jobReferenceNumber']);
   $first_name = test_input($_POST["firstName"]);
   $last_name = test_input($_POST["lastName"]);
@@ -24,62 +26,65 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $phoneNumber = test_input($_POST['phoneNumber']);
   $requiredTechnicalList = test_input($_POST['requiredTechnicalList']);
 
-  if (empty($jobReferenceNumber)) {
-    echo "You must enter your Job Reference Number associated with the position";
-}
-elseif (empty($first_name)) {
-    echo "You must enter your First Name";
-}
-elseif (!preg_match("/^[A-Za-z]+$/", $first_name)) {
-    echo "First Name can only contain letters A–Z.";
-}
-elseif (!preg_match("/^.{1,30}$/", $first_name)) {
-    echo "First name must be between 1 - 30 characters";
-}
-elseif (empty($last_name)) {
-    echo "You must enter your Last Name";
-}
-elseif (!preg_match("/^[A-Za-z]+$/", $last_name)) {
-    echo "Last Name can only contain letters A–Z.";
-}
-elseif (!preg_match("/^.{1,30}$/", $last_name)) {
-    echo "Last Name must be between 1 - 30 characters";
-}
-elseif (empty($dob)) {
-    echo "You must select your Date of Birth";
-}
-elseif (empty($gender)) {
-    echo "You must select a Gender";
-}
-elseif (empty($address)) {
-    echo "An address needs to be entered";
-}
-elseif (!preg_match("/^[0-9]{1,5}[A-Za-z]?\s[A-Za-z\s]{2,50}$/", $address)) {
-    echo "Address needs to be in the form 123 Main Street, 12B Victoria Rd";
-}
-elseif (empty($suburb)) {
-    echo "You must enter a suburb";
-}
-elseif (!preg_match("/^[A-Za-z\s'\-]+$/", $suburb)) {
-    echo "Suburb can only contain letters, spaces, hyphens, or apostrophes.";
-}
-elseif (!preg_match("/^.{2,50}$/", $suburb)) {
-    echo "Suburb must be between 2 and 50 characters long.";
-}
-elseif (empty($state)) {
-    echo "You must select a state";
-}
-elseif (empty($postcode)) {
-    echo "You must enter your postcode";
-}
-elseif (!preg_match("/^[0-9]+$/", $postcode)) {
-    echo "Postcode must be a number.";
-}
-elseif (!preg_match("/^[0-9]{1,4}$/", $postcode)) {
-    echo "Postcode must be between 1 - 4 digits";
-}
-else {
-    echo "Done!";
-}
-}
+  if (empty($jobReferenceNumber)) 
+    {
+        echo "You must enter your Job Reference Number associated with the position";
+        }
+        elseif (empty($first_name)) 
+        {
+            echo "You must enter your First Name";
+        }
+        elseif (!preg_match("/^[A-Za-z]+$/", $first_name)) {
+            echo "First Name can only contain letters A–Z.";
+        }
+        elseif (!preg_match("/^.{1,30}$/", $first_name)) {
+            echo "First name must be between 1 - 30 characters";
+        }
+        elseif (empty($last_name)) {
+            echo "You must enter your Last Name";
+        }
+        elseif (!preg_match("/^[A-Za-z]+$/", $last_name)) {
+            echo "Last Name can only contain letters A–Z.";
+        }
+        elseif (!preg_match("/^.{1,30}$/", $last_name)) {
+            echo "Last Name must be between 1 - 30 characters";
+        }
+        elseif (empty($dob)) {
+            echo "You must select your Date of Birth";
+        }
+        elseif (empty($gender)) {
+            echo "You must select a Gender";
+        }
+        elseif (empty($address)) {
+            echo "An address needs to be entered";
+        }
+        elseif (!preg_match("/^[0-9]{1,5}[A-Za-z]?\s[A-Za-z\s]{2,50}$/", $address)) {
+            echo "Address needs to be in the form 123 Main Street, 12B Victoria Rd";
+        }
+        elseif (empty($suburb)) {
+            echo "You must enter a suburb";
+        }
+        elseif (!preg_match("/^[A-Za-z\s'\-]+$/", $suburb)) {
+            echo "Suburb can only contain letters, spaces, hyphens, or apostrophes.";
+        }
+        elseif (!preg_match("/^.{2,50}$/", $suburb)) {
+            echo "Suburb must be between 2 and 50 characters long.";
+        }
+        elseif (empty($state)) {
+            echo "You must select a state";
+        }
+        elseif (empty($postcode)) {
+            echo "You must enter your postcode";
+        }
+        elseif (!preg_match("/^[0-9]+$/", $postcode)) {
+            echo "Postcode must be a number.";
+        }
+        elseif (!preg_match("/^[0-9]{1,4}$/", $postcode)) {
+            echo "Postcode must be between 1 - 4 digits";
+        }
+        else {
+            echo "Done!";
+        }
+    }
+
 ?>
